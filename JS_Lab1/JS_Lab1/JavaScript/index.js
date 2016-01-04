2720dddf2a448d16c584cc3b8a8e4aeb1fe9bad7
Bjorsch <johanbjork47@hotmail.com>
2016-01-04 15:33:20 +0100

    uppgift 2

--- JS_Lab1/JS_Lab1/JavaScript/index.js
+++ JS_Lab1/JS_Lab1/JavaScript/index.js
@@ -1,4 +1,4 @@
-﻿//uppgift 1
+﻿//Uppgift 1
 var kronorIDollar = MoneyDollar(sekKronor);
 var kronorIPund = MoneyPund(sekKronor);
 
@@ -13,3 +13,29 @@ function MoneyPund(sekKronor) {
     var result = kronor / 12 + ' pund';
     document.getElementById("resultPund").innerHTML = result;
 }
+
+//Uppgift 2
+
+function numbers() {
+    var text = '';
+    var i;
+    for (i = 100; i > 0; i--) {
+        text += i + '<br>';
+    }
+    document.getElementById('result').innerHTML = text;
+}
+
+
+
+
+
+
+
+
+//function ToHigh() {
+//    var tal1 = document.getElementById('nummer1').value;
+//    var tal2 = document.getElementById('nummer2').value;
+//    if (tal1*2 < ) {
+        
+//    }
+//}
