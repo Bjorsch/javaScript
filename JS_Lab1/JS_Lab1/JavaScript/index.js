80ca9078ac11944863542adac470cff1b2ce66b6
Bjorsch <johanbjork47@hotmail.com>
2016-01-04 14:51:47 +0100

    uppgift1

--- JS_Lab1/JS_Lab1/JavaScript/index.js
+++ JS_Lab1/JS_Lab1/JavaScript/index.js
@@ -1,15 +1,15 @@
 ﻿//uppgift 1
-
 var kronorIDollar = MoneyDollar(sekKronor);
-//var kronorIPund = MoneyPund(kronor, pund);
-//document.getElementById("resultDollar").innerHTML = kronorIDollar;
+var kronorIPund = MoneyPund(sekKronor);
 
 function MoneyDollar(sekKronor) {
     var kronor = document.getElementById('pickANumber').value;
-    var kalle = kronor * 6;
-    document.getElementById("resultDollar").innerHTML = kalle;
+    var result = kronor / 6 + ' dollar';
+    document.getElementById("resultDollar").innerHTML = result;
 }
 
-//function MoneyPund(kronor, pund) {
-
-//}
+function MoneyPund(sekKronor) {
+    var kronor = document.getElementById('pickANumber').value;
+    var result = kronor / 12 + ' pund';
+    document.getElementById("resultPund").innerHTML = result;
+}
